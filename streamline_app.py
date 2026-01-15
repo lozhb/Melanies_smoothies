b898b7de48f1817fc79a34504192684c8f55c120
lozhb <blayneylauren@gmail.com>
2026-01-15 09:02:16 +0000

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 st.title(":cup_with_straw: Customise Your Smoothie! :cup_with_straw:")
 st.write(
@@ -16,6 +17,7 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
   'Choose up to 5 ingredients:',
